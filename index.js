67725765f175fdf04b51d50498350ac92c89b91b
Edward Hambleton <ehambleton@me.com>
2018-02-19 21:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function deepestChild() {
 let current = document.querySelector('#grand-node')
   let next = current.children
 for (let i = 0; current.children.length > 0; i++)
-  var current = current.children[0]
+current = current.children[0]
 }
 return current
 }
