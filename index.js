cf00ca864e804d473f87d23a7929f259f32ac8b7
Edward Hambleton <ehambleton@me.com>
2018-02-19 21:55:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  var selection = document.querySelector(selector)
+  return selection
+}
+
+function nestedTarget() {
+  var selection = document.querySelector('#nested .target')
+  return selection;
+}
+
+function increaseRankBy(n) {
+  var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < ranks.length; i++) {
+    var a = ranks[i].innerHTML
+    a = parseInt(a) + n
+    ranks[i].innerHTML = a
+  }
+}
+
+function deepestChild() {
+let current = document.querySelector('#grand-node')
+  let next = current.children
+  while (current.children.length > 0) {
+  let current = next
+}
+return current
+}
