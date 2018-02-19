d66a0ea5be8f397721e1608bd4cc202f6d0b431c
Edward Hambleton <ehambleton@me.com>
2018-02-19 22:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-let current = document.getElementById('grand-node')
+let current = document.querySelector('#grand-node')
   let next = current.children[0]
 while (current.children.length > 0) {
 current = next
